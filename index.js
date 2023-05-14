53dd1d0e9afeb53cff0b94f186cac828306de25a
cheick-dev <114158299+cheick-dev@users.noreply.github.com>
2023-05-14 16:26:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 3000;
 require("./config");
 const { Utilisateur } = require("./user");
 const { Register } = require("./register");
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 app.get("/", async (req, res)=> {
 	res.json({message: "hello"})
 })
