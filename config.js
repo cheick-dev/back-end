ce08004e02114c2fb607d21c2557e9759b4b47e2
cheick-dev <114158299+cheick-dev@users.noreply.github.com>
2023-05-15 10:12:42 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
+try {
+	const uri = "mongodb+srv://cheickdev1010:toto1010@cluster0.s9yvjom.mongodb.net/?retryWrites=true&w=majority";
+	const connection = async () => {
+		await mongoose.connect(uri);
+		console.log("connected");
+	};
+	connection();
+} catch(err) {
+	console.log(err)
+}
 
-const uri = "mongodb+srv://cheickdev1010:toto1010@cluster0.s9yvjom.mongodb.net/?retryWrites=true&w=majority";
-const connection = async () => {
-	await mongoose.connect(uri);
-	console.log("connected");
-};
-connection();
