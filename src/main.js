b1f544e826d81cb7d4a53689a16bb989dd00a58c
cheick <cheickdev1010@gmail.com>
2025-02-14 14:03:32 +0000

    Change function thinks

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { Client, Storage, Users } from 'node-appwrite';
-// import mammoth from 'mammoth';
-// import xlsx from 'xlsx';
-// import path from 'path';
-// import { convertHtmlToPdf } from './convertToPdf';
+import mammoth from 'mammoth';
+import xlsx from 'xlsx';
+import path from 'path';
+import { convertHtmlToPdf } from './convertToPdf';
 
 // Fonction déclenchée par Appwrite (via webhook ou trigger)
 export default async ({ req, res, log, error }) => {
@@ -13,75 +13,75 @@ export default async ({ req, res, log, error }) => {
         .setKey(req.headers['x-appwrite-key'] ?? ''); // Clé d'API Appwrite
 
     const storage = new Storage(client);
-    const users = new Users(client);
-    // const list = await storage.listFiles('67a77cd8000fe5cee78c');
-    const data = { list: null, users: null };
-    try {
-        data['list'] = await storage.listFiles('67a77cd8000fe5cee78c');
-        data['users'] = await users.list();
-    } catch (error) {
-        data['list'] = JSON.stringify(error);
-        data['users'] = JSON.stringify(error);
-        log(error);
-    }
-
+    // const users = new Users(client);
+    // // const list = await storage.listFiles('67a77cd8000fe5cee78c');
+    // const data = { list: null, users: null };
     // try {
-    //     // Récupération des informations sur le fichier uploadé
-    //     const fileId = req.body.$id; // ID du fichier téléchargé
-    //     const fileName = req.body.name;
-    //     const fileExtension = path.extname(fileName).toLowerCase();
-
-    //     log(`Traitement du fichier : ${fileName}`);
-
-    //     // Récupérer le fichier depuis le storage Appwrite
-    //     const fileData = await storage.getFilePreview(fileId);
-
-    //     let pdfBuffer;
-
-    //     // Conversion selon le type de fichier
-    //     if (fileExtension === '.pdf') {
-    //         pdfBuffer = fileData.buffer;
-    //         log(
-    //             `Le fichier ${fileName} est déjà un PDF, aucune conversion nécessaire.`
-    //         );
-    //     } else if (fileExtension === '.docx') {
-    //         const result = await mammoth.convertExcelToHtml({
-    //             buffer: fileData.buffer,
-    //         });
-    //         const htmlContent = result.value;
-    //         pdfBuffer = await convertHtmlToPdf(htmlContent);
-    //         log(`Fichier Word converti en PDF pour ${fileName}`);
-    //     } else if (fileExtension === '.xlsx') {
-    //         const workbook = xlsx.read(fileData.buffer, { type: 'buffer' });
-    //         const sheet = workbook.Sheets[workbook.SheetNames[0]];
-    //         const sheetData = xlsx.utils.sheet_to_json(sheet);
-    //         const htmlContent = convertExcelToHtml(sheetData);
-    //         pdfBuffer = await convertHtmlToPdf(htmlContent);
-    //         log(`Fichier Excel converti en PDF pour ${fileName}`);
-    //     } else {
-    //         log(`Format de fichier non pris en charge : ${fileExtension}`);
-    //         return res.json({
-    //             message: `Le format de fichier ${fileExtension} n'est pas pris en charge.`,
-    //         });
-    //     }
-
-    //     // Télécharger le PDF converti dans le même bucket sans modifier l'ID
-    //     await storage.createFile(
-    //         '67a77cd8000fe5cee78c',
-    //         pdfBuffer,
-    //         fileName.replace(fileExtension, '.pdf')
-    //     );
-    //     log(`Fichier PDF téléchargé avec succès : ${fileName}`);
-
-    //     // Retourner une réponse
-    //     return res.json({
-    //         message: 'Conversion terminée et fichier téléchargé.',
-    //     });
-    // } catch (err) {
-    //     error(`Erreur lors de la conversion des fichiers : ${err.message}`);
+    //     data['list'] = await storage.listFiles('67a77cd8000fe5cee78c');
+    //     data['users'] = await users.list();
+    // } catch (error) {
+    //     data['list'] = JSON.stringify(error);
+    //     data['users'] = JSON.stringify(error);
+    //     log(error);
     // }
 
-    return res.json({ data });
+    try {
+        // Récupération des informations sur le fichier uploadé
+        const fileId = req.body.$id; // ID du fichier téléchargé
+        const fileName = req.body.name;
+        const fileExtension = path.extname(fileName).toLowerCase();
+
+        log(`Traitement du fichier : ${fileName}`);
+
+        // Récupérer le fichier depuis le storage Appwrite
+        const fileData = await storage.getFilePreview(fileId);
+
+        let pdfBuffer;
+
+        // Conversion selon le type de fichier
+        if (fileExtension === '.pdf') {
+            pdfBuffer = fileData.buffer;
+            log(
+                `Le fichier ${fileName} est déjà un PDF, aucune conversion nécessaire.`
+            );
+        } else if (fileExtension === '.docx') {
+            const result = await mammoth.convertExcelToHtml({
+                buffer: fileData.buffer,
+            });
+            const htmlContent = result.value;
+            pdfBuffer = await convertHtmlToPdf(htmlContent);
+            log(`Fichier Word converti en PDF pour ${fileName}`);
+        } else if (fileExtension === '.xlsx') {
+            const workbook = xlsx.read(fileData.buffer, { type: 'buffer' });
+            const sheet = workbook.Sheets[workbook.SheetNames[0]];
+            const sheetData = xlsx.utils.sheet_to_json(sheet);
+            const htmlContent = convertExcelToHtml(sheetData);
+            pdfBuffer = await convertHtmlToPdf(htmlContent);
+            log(`Fichier Excel converti en PDF pour ${fileName}`);
+        } else {
+            log(`Format de fichier non pris en charge : ${fileExtension}`);
+            return res.json({
+                message: `Le format de fichier ${fileExtension} n'est pas pris en charge.`,
+            });
+        }
+
+        // Télécharger le PDF converti dans le même bucket sans modifier l'ID
+        await storage.createFile(
+            '67a77cd8000fe5cee78c',
+            pdfBuffer,
+            fileName.replace(fileExtension, '.pdf')
+        );
+        log(`Fichier PDF téléchargé avec succès : ${fileName}`);
+
+        // Retourner une réponse
+        return res.json({
+            message: 'Conversion terminée et fichier téléchargé.',
+        });
+    } catch (err) {
+        error(`Erreur lors de la conversion des fichiers : ${err.message}`);
+    }
+
+    return res.json({ data: 'ok' });
 };
 
 // import { Client, Storage, InputFile } from 'node-appwrite';
